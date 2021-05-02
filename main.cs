113541b65997e39cbdb53a31b5de9d14a4ed364c
igorantony <>
2021-05-02 18:24:50 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+     int num1, num2, res;
+
+     num1=10;
+     num2=2;
+     res = num1 * num2;
+
+     Console.WriteLine("A soma de " + num1 + " mais " + num2 + " e igual a: " + res);
+    }
+  }
